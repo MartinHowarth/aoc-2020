47153f59417fb9d3ad1c64c95a74cd7e7d7a7577
Martin Howarth <martin.howarth@metaswitch.com>
2020-12-01 11:31:58 +0000

    rustfmt

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -43,16 +43,18 @@ fn part2() {
     }
 }
 
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-
 fn read_numbers<P>(filename: P) -> Vec<i32>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let mut result: Vec<i32> = Vec::new();
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
